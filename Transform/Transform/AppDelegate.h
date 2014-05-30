45225f9e441e50beb979db5b3203a729a6b808b8
Kevin Liang <keviekev23@gmail.com>
2014-05-30 16:41:25 -0700

    created empty object.

---
+++ Transform/Transform/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Transform
+//
+//  Created by Kevin Liang on 5/30/14.
+//  Copyright (c) 2014 Code4Kingdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Transform/Transform/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Transform
+//
+//  Created by Kevin Liang on 5/30/14.
+//  Copyright (c) 2014 Code4Kingdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
