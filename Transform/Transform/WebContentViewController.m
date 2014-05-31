c08c3f0968f955242797a01a6853df9a04a9edd8
Kevin Liang <keviekev23@gmail.com>
2014-05-31 13:38:25 -0700

    load gather and prayer pages separately.

---
+++ Transform/Transform/WebContentViewController.m
@@ -0,0 +1,71 @@
+//
+//  WebContentViewController.m
+//  Transform
+//
+//  Created by Kevin Liang on 5/31/14.
+//  Copyright (c) 2014 Code4Kingdom. All rights reserved.
+//
+
+#import "WebContentViewController.h"
+
+@interface WebContentViewController ()
+
+@end
+
+@implementation WebContentViewController
+
+@synthesize webView = _webView;
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) loadCurrentLocalUrl:(NSString *)url
+{
+    NSString *filename = [[NSBundle mainBundle] pathForResource:url ofType:@"html"];
+    NSString *path = [[NSBundle mainBundle] bundlePath];
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+    NSString* htmlString = [NSString stringWithContentsOfFile:filename encoding:NSUTF8StringEncoding error:nil];
+    [self.webView loadHTMLString:htmlString baseURL:baseURL];
+}
+
+- (void) loadJavascript:(NSString *)url
+{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:url ofType:@"js"];
+    if (filePath) {
+        NSString *script = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+        [self.webView stringByEvaluatingJavaScriptFromString:script];
+    }
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
