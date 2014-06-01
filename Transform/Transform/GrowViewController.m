dc02c9a0e6d0e868cbb120a428ee18c2b5232474
Kevin Liang <keviekev23@gmail.com>
2014-05-31 21:15:18 -0700

    added grow tab.

---
+++ Transform/Transform/GrowViewController.m
@@ -0,0 +1,52 @@
+//
+//  GrowViewController.m
+//  Transform
+//
+//  Created by Kevin Liang on 5/31/14.
+//  Copyright (c) 2014 Code4Kingdom. All rights reserved.
+//
+
+#import "GrowViewController.h"
+
+@interface GrowViewController ()
+
+@end
+
+@implementation GrowViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.title = @"Grow";
+    [self loadCurrentLocalUrl:@"grow"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
